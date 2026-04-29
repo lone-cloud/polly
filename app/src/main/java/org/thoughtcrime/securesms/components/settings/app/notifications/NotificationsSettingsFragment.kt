bc07fc51eb3a20cfc1e015cfe5e65d2a0e0340f1
lone-cloud <lonecloud604@pm.me>
2026-04-29 13:54:26 -0700

    Polly: fork customizations on top of Molly 6186d1e071

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/notifications/NotificationsSettingsFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/notifications/NotificationsSettingsFragment.kt
@@ -61,6 +61,7 @@ import org.signal.core.ui.compose.Scaffolds
 import org.signal.core.ui.compose.SignalIcons
 import org.signal.core.ui.compose.Texts
 import org.signal.core.util.logging.Log
+import org.thoughtcrime.securesms.BuildConfig
 import org.thoughtcrime.securesms.R
 import org.thoughtcrime.securesms.components.PromptBatterySaverDialogFragment
 import org.thoughtcrime.securesms.components.settings.app.routes.AppSettingsRoute
@@ -704,7 +705,7 @@ fun NotificationsSettingsScreen(
 
       val notificationMethods = NotificationDeliveryMethod.entries.filter { method ->
         when (method) {
-          NotificationDeliveryMethod.FCM -> true
+          NotificationDeliveryMethod.FCM -> !BuildConfig.HIDE_FCM_OPTION
           NotificationDeliveryMethod.WEBSOCKET -> true
           NotificationDeliveryMethod.UNIFIEDPUSH -> !state.isLinkedDevice
         }
